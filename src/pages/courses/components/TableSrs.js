9515139248d5ac01d8aa00951fb10c809a56c637
QNhatCNTT <nhattrum1998@gmail.com>
2021-07-20 22:38:48 +0700

    delete course

--- src/pages/courses/components/TableSrs.js
+++ src/pages/courses/components/TableSrs.js
@@ -4,7 +4,7 @@ import { Table, Form, Input, Button, Select, Modal } from 'antd';
 import 'antd/dist/antd.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCourses, deleteCourses, putCourses } from '../../../redux/courses/coursesAction';
-
+import * as moment from 'moment';
 
 export default function TableSrc(props) {
 
@@ -28,33 +28,41 @@ export default function TableSrc(props) {
     setPage(pagination.current)
   }
 
-  const onDelete = (idCategory) => {
-    dispatch(deleteCourses(idCategory))
+  const onDelete = (coursesID) => {
+    dispatch(deleteCourses(coursesID))
   }
-
-  const onEdit = (idCategory) => {
-    setCategoryIdUpdate(idCategory)
-    setVisible(true);
-  }
-
   const columns = [
     {
-      title: 'Level courses',
+      title: 'Name courses',
       dataIndex: 'courseName',
       key: 'courseName',
       sorter: true,
     },
     {
-      title: 'Category Name',
+      title: 'Fee',
       dataIndex: 'fee',
       key: 'fee',
       sorter: true,
     },
     {
-      title: 'Category Name',
+      title: 'Teacher',
+      dataIndex: 'teacher',
+      key: 'teacher.id',
+      sorter: true,
+      render: (teacher) => <div>{teacher.firstName + ' ' + teacher.lastName}</div>
+    },
+    {
+      title: 'Number student',
       dataIndex: 'studies',
       key: 'studies',
       sorter: true,
+    },
+    {
+      title: 'Date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      sorter: true,
+      render: (createdAt) => <div>{moment(createdAt).format('DD/MM/YYYY')}</div>
     },
     {
       title: 'Action',
@@ -62,30 +70,9 @@ export default function TableSrc(props) {
       render: (id) =>
         <div>
           <a style={{ cursor: 'pointer', color: '#ff6666', marginRight: 20 }} onClick={() => onDelete(id)}>delete</a>
-          <a style={{ cursor: 'pointer' }} onClick={() => onEdit(id)}>edit</a>
         </div>
     },
   ];
-
-  const [visible, setVisible] = useState(false);
-
-  const handleCancel = () => {
-    setVisible(false);
-  };
-
-  const onFinish = (values) => {
-    dispatch(putCourses(
-      {
-        'coursesId': coursesIdUpdate,
-        'coursesName': values.coursesName,
-        'levelCategory': values.levelCategory
-      }))
-    setVisible(false);
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    setVisible(true);
-  };
 
   return (
     <div>
@@ -94,52 +81,6 @@ export default function TableSrc(props) {
         pageSize: 5,
         total: totalCategory
       }} />
-      <Modal
-        title="Update courses"
-        visible={visible}
-        onCancel={handleCancel}
-        okButtonProps={{
-          style: {
-            display: "none",
-          },
-        }}
-        cancelButtonProps={{
-          style: {
-            display: "none",
-          },
-        }}
-      >
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            label="Level courses"
-            name="levelCategory"
-            rules={[{ required: true, message: 'Please input level courses!' }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Category name"
-            name="coursesName"
-            rules={[{ required: true, message: 'Please input courses name!' }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Update
-            </Button>
-          </Form.Item>
-        </Form>
-      </Modal>
     </div>
   )
 }
